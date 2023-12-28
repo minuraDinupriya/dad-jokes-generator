547a7dcd1111f983f862cd0dffd9d93cc8b8e2d9
Minura Dinupriya <mranaweera793@gmail.com>
2023-12-28 20:50:14 +0530

    js finished with try-catch...all done

--- asset/js/app.js
+++ asset/js/app.js
@@ -1,20 +1,36 @@
-const btnEl=document.getElementById("btn");
+const btnEl = document.getElementById("btn");
+const jokeEl = document.getElementById("joke");
 
-const apiKey="vpOdUSK6odOrmbmoNc1zEw==he9DhuyK3ZD7lAHS";
+const apiKey = "vpOdUSK6odOrmbmoNc1zEw==he9DhuyK3ZD7lAHS";
 
-const options={
-    method: "GET",
-    headers: {
-        "X-Api-Key": apiKey,
-    },
+const options = {
+  method: "GET",
+  headers: {
+    "X-Api-Key": apiKey,
+  },
 };
 
-const apiUrl="https://api.api-ninjas.com/v1/dadjokes?limit=1";
+const apiUrl = "https://api.api-ninjas.com/v1/dadjokes?limit=1";
 
-async function getJoke(){
-    const response=await fetch(apiUrl, options);
-    const data=await response.json();
-    console.log(data);
+async function getJoke() {
+  try {
+    jokeEl.innerHTML = "Updating...";
+    btnEl.disabled = true;
+    btnEl.innerText = "Loading...";
+
+    const response = await fetch(apiUrl, options);
+    const data = await response.json();
+    jokeEl.innerHTML = data[0].joke;
+
+    btnEl.disabled = false;
+    btnEl.innerText = "Tell me a Joke";
+
+  } catch(error) {
+    jokeEl.innerHTML = "An error occcured... try again later";
+    
+    btnEl.disabled = false;
+    btnEl.innerText = "Tell me a Joke";
+  }
 }
 
-btnEl.addEventListener("click",getJoke);
+btnEl.addEventListener("click", getJoke);
